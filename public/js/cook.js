6073b697d708ffb88a88bbf1f4a8b2ab4a4fb9d3
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2022-05-23 20:47:54 +0200

    pierwszy udany JS... no prawie

--- public/js/cook.js
+++ public/js/cook.js
@@ -1,1 +1,10 @@
-console.log("abc")
+function addOrderToList(){
+    const templateOrder = document.querySelector('#order-div');
+    const container = document.querySelector('.content-cook-container');
+    const clone = templateOrder.content.cloneNode(true);
+
+    const nrZam = clone.querySelector("div").querySelector("h1");
+    console.log(nrZam);
+    nrZam.innerHTML = "2";
+   container.appendChild(clone);
+}
