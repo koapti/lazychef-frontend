8e66d183e08fa964c92fe9dd9f8379c5c4826d9f
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2022-05-23 20:58:15 +0200

    Zamówienia dodaja sie fest

--- public/js/cook.js
+++ public/js/cook.js
@@ -1,10 +1,20 @@
 function addOrderToList(){
     const templateOrder = document.querySelector('#order-div');
+    const templateAtomicOrder = document.querySelector('#atomic-order-div');
     const container = document.querySelector('.content-cook-container');
     const clone = templateOrder.content.cloneNode(true);
 
     const nrZam = clone.querySelector("div").querySelector("h1");
-    console.log(nrZam);
-    nrZam.innerHTML = "2";
+    
+    nrZam.innerHTML = "2"; // tutaj wstawienie numeru zamowienia
+
+    //tu for po atomic orderach i dla każdego zrobienie klona, uzupenienie go i dodanie
+    const atomicOrderClone = templateAtomicOrder.content.cloneNode(true);
+    const nazwaDania = atomicOrderClone.querySelector("div").querySelector("h2");
+
+    nazwaDania.innerHTML = "Kotlet plus fryty";
+
+    clone.querySelector("div").appendChild(atomicOrderClone);
+
    container.appendChild(clone);
 }
