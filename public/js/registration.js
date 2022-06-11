c0eaf65eafab38a51dcfadd4866626966d76c038
Guurkha <procode38@gmail.com>
2022-06-11 13:26:53 +0200

    refactored

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,4 +1,3 @@
-alert('dupa');
 const form = document.querySelector("form");
 const emailInput = form.querySelector('input[name="email"]');
 const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
